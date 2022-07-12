710bd638d7411f7a16610bd11864855ffa94293b
Antonio Codella <codellaantonio1992@gmail.com>
2022-07-12 15:02:08 +0200

    add-prettier

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const { createServer } = require("node:http");
+
+function createApp() {
+  return createServer((request, response) => {
+    console.log("request received");
+
+    response.statusCode = 200;
+
+    response.setHeader("Content-Type", "application/json");
+
+    const jsonResponseBody = JSON.stringify({ location: "Earth" });
+
+    response.end(jsonResponseBody);
+  });
+}
+
+module.exports = createApp;
