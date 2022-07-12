3c62149e6cbe1a9d59366a24d628e890bd17108c
Antonio Codella <codellaantonio1992@gmail.com>
2022-07-12 16:24:20 +0200

    createServer

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import "express-async-errors";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Up and Running!");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`[server]: Server in running at http://localhost${port}!`);
+});
--- src/server2.ts
+++
@@ -1,3 +0,0 @@
-const name: string = "Antonio";
-
-console.log(`Hello ${name}`);
