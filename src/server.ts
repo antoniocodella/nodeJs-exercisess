114b3b6f26307febb5820d9c7b8d0b54b8f09b99
Antonio Codella <codellaantonio1992@gmail.com>
2022-07-12 16:35:35 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ app.get("/", (request, response) => {
     response.send("Up and Running!");
 });
 
-const port = 3000;
+const port = 3001;
 
 app.listen(port, () => {
     console.log(`[server]: Server in running at http://localhost${port}!`);
