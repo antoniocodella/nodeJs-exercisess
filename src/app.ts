141d2ec5aea8e3a2b21e2ce3410143d462919d5c
Antonio Codella <codellaantonio1992@gmail.com>
2022-07-13 18:20:19 +0200

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,18 @@ import "express-async-errors";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Things!");
+app.get("/fruits", (request, response) => {
+    response.json({
+        nome: "Mela",
+        genere: "Malus",
+        famiglia: "Rosaceae",
+        calorie: 52,
+        carboidrati: 11,
+        grassi: 1,
+        proteine: 1,
+        zuccheri: 10,
+        prezzo: 2,
+    });
 });
 
 export default app;
