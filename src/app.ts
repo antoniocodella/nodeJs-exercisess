15e369463b163fbcc67878566ea129472cd5dcdf
Antonio Codella <codellaantonio1992@gmail.com>
2022-07-15 18:28:03 +0200

    installing cors

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,68 @@
 import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client";
+import cors from "cors";
 
 const app = express();
 
-app.get("/fruits", (request, response) => {
-    response.json({
-        nome: "Mela",
-        genere: "Malus",
-        famiglia: "Rosaceae",
-        calorie: 52,
-        carboidrati: 11,
-        grassi: 1,
-        proteine: 1,
-        zuccheri: 10,
-        prezzo: 2,
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/fruits", async (request, response) => {
+    const fruits = await prisma.fruit.findMany();
+
+    response.json(fruits);
+});
+
+app.get("/fruits/:id(\\d+)", async (request, response) => {
+    const fruitId = Number(request.params.id);
+
+    const fruit = await prisma.fruit.findUnique({
+        where: { id: fruitId },
     });
+
+    response.json(fruit);
+});
+
+app.post("/fruits", async (request, response) => {
+    const fruitData = request.body;
+
+    const fruit = await prisma.fruit.create({ data: fruitData });
+
+    response.status(201).json(fruit);
+});
+
+app.put("/fruits/:id(\\d+)", async (request, response, next) => {
+    const fruitData = request.body;
+    const fruitId = Number(request.params.id);
+
+    try {
+        const fruit = await prisma.fruit.update({
+            where: { id: fruitId },
+            data: fruitData,
+        });
+
+        response.status(200).json(fruit);
+    } catch (error) {
+        response.status(404);
+        next(`Cannot PUT /fruits/${fruitId}`);
+    }
+});
+
+app.delete("/fruits/:id(\\d+)", async (request, response, next) => {
+    const fruitId = Number(request.params.id);
+
+    try {
+        await prisma.fruit.delete({
+            where: { id: fruitId },
+        });
+
+        response.status(200).end();
+    } catch (error) {
+        response.status(404);
+        next(`Cannot DELETE /fruits/${fruitId}`);
+    }
 });
 
 export default app;
