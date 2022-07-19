0d9ab15d03c92c04735f3d2f31437a0159376d0a
Antonio Codella <codellaantonio1992@gmail.com>
2022-07-19 17:54:13 +0200

    add photofilename to prisma

---
+++ prisma/migrations/20220719155119_photo/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Fruit" ADD COLUMN     "photoFilename" VARCHAR(255);
--- src/lib/middleware/multer.ts
+++ src/lib/middleware/multer.ts
@@ -1,8 +1,26 @@
 import multer from "multer";
+import mime from "mime";
+import { randomUUID } from "node:crypto";
+import { request } from "node:http";
+
+export const generatePhotoFilename = (mimetype: string) => {
+    const randomFilename = `${randomUUID()}-${Date.now()}`;
+    const fileExtensions = mime.getExtension(mimetype);
+    const filename = `${randomFilename}.${fileExtensions}`;
+
+    return filename;
+};
+
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: (request, file, callback) => {
+        return callback(null, generatePhotoFilename(file.mimetype));
+    },
+});
 
 export const multerOptions = {};
 
 export const initMulterMiddleware = () => {
-    return multer(multerOptions);
+    return multer({ storage, ...multerOptions });
 };
 
